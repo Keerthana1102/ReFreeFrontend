6c1d7f753d44edf0578898f92844fa589d1e7ecc
Prernaswarna <prernaswarnakumar@gmail.com>
2020-10-21 16:18:31 +0530

    Formatting changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,26 +15,30 @@ class Login extends Component
 	{
 		return(
 			<div style={{margin:'auto' , width:'50%'}}>
-			<br />
-			<br />
-			<div style={{display:'grid' , justifyContent:'center'}}><Icon name='globe' size='huge' /></div>
-			<h2 style={{textAlign:'center'}}>Sign In to ReFree</h2>
-			<Segment>
-				<Form>
-					<Form.Field>
-						<label>Username</label>
-						<input type='text'  />
-					</Form.Field>
-					<Form.Field>
-						<label>Password</label>
-						<input type='password' />
-					</Form.Field>
-					<div style={{textAlign:'center'}}><Button fluid color='green' type='submit'>Sign In</Button></div>
-				</Form>
-			</Segment>
-			<Message textAlign='center'>
-				New to ReFree? <Link to="/signup">Create an account</Link>
-			</Message>
+				<br />
+				<br />
+				<div style={{display:'grid' , justifyContent:'center'}}>
+					<Icon name='globe' size='huge' />
+				</div>
+				<h2 style={{textAlign:'center'}}>Sign In to ReFree</h2>
+				<Segment>
+					<Form>
+						<Form.Field>
+							<label>Username</label>
+							<input type='text'  />
+						</Form.Field>
+						<Form.Field>
+							<label>Password</label>
+							<input type='password' />
+						</Form.Field>
+						<div style={{textAlign:'center'}}>
+							<Button fluid color='green' type='submit'>Sign In</Button>
+						</div>
+					</Form>
+				</Segment>
+				<Message textAlign='center'>
+					New to ReFree? <Link to="/signup">Create an account</Link>
+				</Message>
 			</div>	
 		);
 	}
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -15,61 +15,63 @@ class Signup extends Component
         {
                 return(
 			<div>
-			<Grid padded>
-			<Grid.Row color='black'>
-			<Grid.Column width={1}>
-			<Icon name="world" size="big"/>
-			</Grid.Column>
-			<Grid.Column width={15}>
-			<h2>ReFree</h2>
-			</Grid.Column>
-			</Grid.Row>
-			</Grid>
+				<Grid padded>
+					<Grid.Row color='black'>
+						<Grid.Column width={1}>
+							<Icon name="world" size="big"/>
+						</Grid.Column>
+						<Grid.Column width={15}>
+							<h2>ReFree</h2>
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
 			<Grid celled>                        
-                        <Grid.Row>
-			<Grid.Column width={6}>
-			<span>
-			<h2>Built for Designers</h2>
-			<p style={{fontSize:'20px'}}>ReFree is a design platform inspired by the way you work. From managing projects to getting hired you can connect with the largest network of desingers on ReFree</p>
-			</span>
-			</Grid.Column>
-                        <Grid.Column width={10}>
-			<h2 style={{textAlign:'center'}}>Sign In to ReFree</h2>
-                        <Segment>
-                                <Form>
-                                        <Form.Field>
-                                                <label>Username</label>
-                                                <input type='text'  />
-                                        </Form.Field>
-					<Form.Field>
-						<label>First Name</label>
-						<input type="text" />
-					</Form.Field>
-					<Form.Field>
-						<label>Last Name</label>
-						<input type="text" />
-					</Form.Field>
-					<Form.Field>
-						<label>Email ID</label>
-						<input type="email" />
-					</Form.Field>
-					<Form.Field>
-						<label>Mobile No</label>
-						<input type="text" />
-					</Form.Field>
-                                        <Form.Field>
-                                                <label>Password</label>
-                                                <input type='password' />
-                                        </Form.Field>
-                                        <div style={{textAlign:'center'}}><Button fluid color='green' type='submit'>Sign Up</Button></div>
-                                </Form>
-                        </Segment>
-                        <Message textAlign='center'>
-                                Already have an account? <Link to="/">Log In</Link>
-</Message>
-			</Grid.Column>
-			</Grid.Row>
-			</Grid>
+                        	<Grid.Row>
+					<Grid.Column width={6}>
+						<span>
+							<h2>Built for Designers</h2>
+							<p style={{fontSize:'20px'}}>ReFree is a design platform inspired by the way you work. From managing projects to getting hired you can connect with the largest network of desingers on ReFree</p>
+						</span>
+					</Grid.Column>
+                        		<Grid.Column width={10}>
+					<h2 style={{textAlign:'center'}}>Sign In to ReFree</h2>
+                        		<Segment>
+                                		<Form>
+                                        		<Form.Field>
+                                                		<label>Username</label>
+                                                		<input type='text'  />
+                                        		</Form.Field>
+							<Form.Field>
+								<label>First Name</label>
+								<input type="text" />
+							</Form.Field>
+							<Form.Field>
+								<label>Last Name</label>
+								<input type="text" />
+							</Form.Field>
+							<Form.Field>
+								<label>Email ID</label>
+								<input type="email" />
+							</Form.Field>
+							<Form.Field>
+								<label>Mobile No</label>
+								<input type="text" />
+							</Form.Field>
+                                        		<Form.Field>
+                                                		<label>Password</label>
+                                                		<input type='password' />
+                                        		</Form.Field>
+                                        	<	div style={{textAlign:'center'}}>
+								<Button fluid color='green' type='submit'>Sign Up</Button>
+							</div>
+                                		</Form>
+                       				</Segment>
+                        			<Message textAlign='center'>
+                                			Already have an account? <Link to="/">Log In</Link>
+						</Message>
+					</Grid.Column>
+					</Grid.Row>
+				</Grid>
 			</div>
                 );
         }
