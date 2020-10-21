d68dd970257f98fae435d68a43f4b016cc5efa22
Prernaswarna <prernaswarnakumar@gmail.com>
2020-10-21 16:43:00 +0530

    Passowrd

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,44 +4,44 @@ import { Link } from 'react-router-dom';
 
 class Login extends Component
 {
-	constructor()
-	{
-		super();
-		this.state = {
-			isLoggedIn : false,
-		}
-	}
-	render()
-	{
-		return(
-			<div style={{margin:'auto' , width:'50%'}}>
-				<br />
-				<br />
-				<div style={{display:'grid' , justifyContent:'center'}}>
-					<Icon name='globe' size='huge' />
-				</div>
-				<h2 style={{textAlign:'center'}}>Sign In to ReFree</h2>
-				<Segment>
-					<Form>
-						<Form.Field>
-							<label>Username</label>
-							<input type='text'  />
-						</Form.Field>
-						<Form.Field>
-							<label>Password</label>
-							<input type='password' />
-						</Form.Field>
-						<div style={{textAlign:'center'}}>
-							<Button fluid color='green' type='submit'>Sign In</Button>
-						</div>
-					</Form>
-				</Segment>
-				<Message textAlign='center'>
-					New to ReFree? <Link to="/signup">Create an account</Link>
-				</Message>
-			</div>	
-		);
-	}
+  constructor()
+  {
+    super();
+    this.state = {
+      isLoggedIn : false,
+    }
+  }
+  render()
+  {
+    return(
+      <div style={{margin:'auto' , width:'50%'}}>
+        <br />
+        <br />
+        <div style={{display:'grid' , justifyContent:'center'}}>
+          <Icon name='globe' size='huge' />
+        </div>
+        <h2 style={{textAlign:'center'}}>Sign In to ReFree</h2>
+        <Segment>
+          <Form>
+            <Form.Field>
+              <label>Username</label>
+              <input type='text'  />
+            </Form.Field>
+            <Form.Field>
+               <label>Password</label>
+               <input type='password' />
+            </Form.Field>
+            <div style={{textAlign:'center'}}>
+              <Button fluid color='green' type='submit'>Sign In</Button>
+            </div>
+          </Form>
+        </Segment>
+        <Message textAlign='center'>
+          New to ReFree? <Link to="/signup">Create an account</Link>
+        </Message>
+      </div>	
+    );
+  }
 }
 
 export default Login;
