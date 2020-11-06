ab68d8d9a75b4ca72edf88d22c16247bd912a2ba
Ramya7894 <ramyapendyala05@gmail.com>
2020-11-06 16:23:08 +0530

    change

--- src/components/Mainpage/Trendingdesigns.js
+++ src/components/Mainpage/Trendingdesigns.js
@@ -3,45 +3,53 @@ import React, { Component } from 'react';
 import "./Trendingdesigns.css";
 import Designcard from './Designcard';
 import axios from 'axios';
-const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/projects/'
-})
+
+axios.defaults.xsrfCookieName = 'frontend_csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+
+
  class Trendingdesigns extends Component {
-    state = {
-        projects: [],
-        users: {}
-    }
+     
     constructor() {
         super();
-        api.get('/', {
-            auth : {
-                username : 'Ramya',
-                password : 'Saira@05'
-            }
-        }).then(res => {
-            console.log(res.data.results)
-            this.setState({projects : res.data.results})
-        })
+        this.state = { users: {}, projects: [], userId : ""}
         axios.get('http://127.0.0.1:8000/users/').then((r) => {
        this.setState({users : r.data.results.reduce((acc, value) => {
          acc[value.id] = value
          return acc
        }, {})})
     })
+    }
+        async componentDidMount()
+        {
+      	const res = await axios({url:'http://127.0.0.1:8000/users/currentuser/', method:'get' , withCredentials:true}).then(response=>{return response}).catch(error=>{window.location.href="http://127.0.0.1:3000/fail"})
 
+        const js =  await res.data;
+        this.setState({userId:js.userId});
+        if(this.state.userId==0)
+        {
+                
+                window.location.href="http://127.0.0.1:3000/";
+        }
+    	const response = await axios(
+        {url:'http://127.0.0.1:8000/projects/',method:'GET' ,  withCredentials:true }).then(response=>{return response}
+        )
+        const json = response.data.results;
+        this.setState({projects:json});
 
     }
-
-
-
+    
+	  
     render() {
         return (
+            
             <div className = "trendingdesigns">
             <h1>Trending Designs</h1>
             <div className = "trendingdesigns_designs">
-            {this.state.projects.map(project => <div>
-            <Designcard  projectname = {project.name} description={project.description} designername={this.state.users[project.user].username} likes={project.likes}/>
-            </div>)}
+            {this.state.projects.map(project =><div>
+            <Designcard  projectname = {project.name} description={project.description} designername = {this.state.users[1].username} likes={project.likes}/>
+            </div>)
+            }
             </div>
 
             </div>
